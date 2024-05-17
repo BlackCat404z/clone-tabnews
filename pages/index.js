802d08d6f11eea5897f6f27cefb1e44de7533200
BlackCat404z <404lifez@gmail.com>
2024-05-17 16:50:46 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Gabriela,eu gosto muito de você. Se você gosta de min, dá uma risadinha ! </h1>
+    return <h1>Gabriela,eu gosto muito de você. Se você gosta de min, dá uma risadinha ! ❤ </h1>
 }
 
 export default Home;
